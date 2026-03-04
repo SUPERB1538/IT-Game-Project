c06fb9c5689619f7cf216fc0d80891adde412a19
Bingze Wu <3075354w@student.gla.ac.uk>
2026-03-05 05:01:28 +0800

    111

---
+++ target/scala-2.13/routes/main/controllers/routes.java
@@ -0,0 +1,20 @@
+// @GENERATOR:play-routes-compiler
+// @SOURCE:/Users/wubingze/Desktop/workspace_java_backend/UofG/IT-Game-Project/conf/routes
+// @DATE:Thu Feb 26 07:34:42 CST 2026
+
+package controllers;
+
+import router.RoutesPrefix;
+
+public class routes {
+  
+  public static final controllers.ReverseGameScreenController GameScreenController = new controllers.ReverseGameScreenController(RoutesPrefix.byNamePrefix());
+  public static final controllers.ReverseAssets Assets = new controllers.ReverseAssets(RoutesPrefix.byNamePrefix());
+
+  public static class javascript {
+    
+    public static final controllers.javascript.ReverseGameScreenController GameScreenController = new controllers.javascript.ReverseGameScreenController(RoutesPrefix.byNamePrefix());
+    public static final controllers.javascript.ReverseAssets Assets = new controllers.javascript.ReverseAssets(RoutesPrefix.byNamePrefix());
+  }
+
+}
